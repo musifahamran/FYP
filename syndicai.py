3ad212c1278e21f262a33e151d8ebaf9760e1293
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-04-06 09:07:45 +0800

    Create syndicai.py

---
+++ syndicai.py
@@ -0,0 +1,26 @@
+import torch
+from PIL import Image 
+from helpers import draw_box, url_to_img, img_to_bytes
+
+
+class PythonPredictor:
+
+    def __init__(self, config):
+        """ Download pretrained model. """
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).autoshape()
+
+    def predict(self, payload):
+        """ Run a model based on url input. """
+
+        # Inference
+        img = url_to_img(payload["url"])
+        results = self.model(img)
+
+        # Draw boxes
+        boxes = results.xyxy[0].numpy()
+        box_img = draw_box(img, boxes)
+
+        # Save image
+        #box_img.save("sample_data/output.png", "PNG")
+
+        return img_to_bytes(box_img)
