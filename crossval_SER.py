f9143c9d8359398de82120ca3279fd202f77f5be
musifahamran <49061096+musifahamran@users.noreply.github.com>
2021-10-09 14:27:17 +0800

    Update crossval_SER.py

--- crossval_SER.py
+++ crossval_SER.py
@@ -4,19 +4,22 @@ import sys
 import pickle
 import os
 
-repeat_kfold = 8 # to  perform k-fold for n-times with different seed
+repeat_kfold = 5 # to  perform k-fold for n-times with different seed
 
 #------------PARAMETERS---------------#
 
 features_file = 'IEMOCAP_logspec200.pkl'
 ser_model     = 'alexnet_gap'
+#IEMOCAP Val and Test ID
+#Comment out when using THAI SER
+val_id =  ['1F','1M','2F','2M','3F','3M','4F','4M','5F','5M']
+test_id = ['1M','1F','2M','2F','3M','3F','4M','4F','5M','5F']
 
-#val_id =  ['1F','1M','2F','2M','3F','3M','4F','4M','5F','5M']
-#test_id = ['1M','1F','2M','2F','3M','3F','4M','4F','5M','5F']
-val_id = ['studio1-10V', 'studio1-10T','studio11-20V','studio11-20T', 'studio21-30V','studio21-30T','studio31-40V','studio31-40T',
-          'studio41-50V','studio41-50T','studio51-60V','studio51-60T','studio61-70V','studio61-70T','studio71-80V', 'studio71-80T']
-test_id = [ 'studio1-10T','studio1-10V','studio11-20T','studio11-20V', 'studio21-30T','studio21-30V','studio31-40T','studio31-40V',
-          'studio41-50T','studio41-50V','studio51-60T','studio51-60V','studio61-70T','studio61-70V', 'studio71-80T','studio71-80V']
+#THAI SER Val aand Test ID, uncomment when using THAI SER
+# val_id = ['studio1-10V', 'studio1-10T','studio11-20V','studio11-20T', 'studio21-30V','studio21-30T','studio31-40V','studio31-40T',
+#           'studio41-50V','studio41-50T','studio51-60V','studio51-60T','studio61-70V','studio61-70T','studio71-80V', 'studio71-80T']
+# test_id = [ 'studio1-10T','studio1-10V','studio11-20T','studio11-20V', 'studio21-30T','studio21-30V','studio31-40T','studio31-40V',
+#           'studio41-50T','studio41-50V','studio51-60T','studio51-60V','studio61-70T','studio61-70V', 'studio71-80T','studio71-80V']
 num_epochs  = '100'
 batch_size  = '64'
 lr          = '0.0001'
